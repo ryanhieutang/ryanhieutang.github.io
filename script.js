8aff9ad6c7047f050a391c5df5d86752d956940a
Ryan Tang <ryanhieutang@gmail.com>
2024-01-29 16:39:05 +1100

    Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function displayAESTTime() {
-    // Create a new Date object representing the current time in Australia/Sydney time zone
     var auDate = new Date().toLocaleString("en-US", { timeZone: "Australia/Sydney" });
-  
-    // Get the HTML element with the id 'aest-time' and update its text content
-    document.getElementById('aest-time').innerText = auDate;
-  
-    // Schedule the displayAESTTime function to be called again after 1000 milliseconds (1 second)
-    setTimeout(displayAESTTime, 1000);
+      document.getElementById('time').innerText = auDate;
+      setTimeout(displayAESTTime, 1000);
   }
-  
-  // Call the displayAESTTime function when the page has finished loading
   window.onload = displayAESTTime;
