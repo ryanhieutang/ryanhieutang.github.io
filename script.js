084a3bef48b2ea6f4b700046a4f240e88c96d65c
Ryan Tang <ryanhieutang@gmail.com>
2024-01-29 16:46:48 +1100

    Fixed time to AEST

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function displayAESTTime() {
     var auDate = new Date().toLocaleString("en-US", { timeZone: "Australia/Sydney" });
-      document.getElementById('time').innerText = auDate;
+      document.getElementById('ausTime').innerText = auDate;
       setTimeout(displayAESTTime, 1000);
   }
   window.onload = displayAESTTime;
